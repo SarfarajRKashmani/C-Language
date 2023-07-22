753a1dca47f722e2de3ec662322b6d69d7aa9032
SarfarajRKashmani <sarfrajkashmani5@gmail.com>
2023-07-22 17:50:12 +0530

    SK | 22/07/2023 | #Ch: 1 Intro to C

---
+++ Ch-1/JC6-1-1.C
@@ -0,0 +1,12 @@
+//Standard Input Output Header File
+#include<stdio.h>
+//Console Input Output Header File
+#include<conio.h>
+
+main()
+{
+	clrscr();
+	printf("Hello\nWorld !!\n\n");
+	printf("Rudra Lakhani");
+	getch();
+}
